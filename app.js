3ce7137e2c9bcdddb3327431bc529b5ec7bfa27a
niko4indo <niko4indo@gmail.com>
2026-05-17 21:29:39 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+{
+"name":"HITIT",
+"short_name":"HITIT",
+"display":"standalone",
+"start_url":"./index.html",
+"background_color":"#f5f5f5",
+"theme_color":"#111111",
+"icons":[
+{"src":"assets/icon-192.png","sizes":"192x192","type":"image/png"},
+{"src":"assets/icon-512.png","sizes":"512x512","type":"image/png"}
+]
+}
